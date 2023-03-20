2e43cf96fb8af1c4b1981c5e3f3929dac111145c
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-03-20 17:50:13 -0500

    agg todo.js

---
+++ src/components/todo.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export default function Todo({item,onUpdate}){
+    // modificar
+    const [isEdit, setIsEdit] = useState(false);
+
+    
+    function FormEdit(){
+        const [newValue,setNewValue] = useState(item.title);
+
+        function handleSubmit(e){
+            e.preventDefault();  
+        }
+    
+        function handleChange(e){
+            const value = e.target.value;
+            setNewValue(value);
+        }
+    
+        function handleClick(){
+            // actulizar nuevo estado
+            onUpdate(item.id, newValue);
+            setIsEdit(false);
+        }
+        
+        return( 
+        <form className="todoUpdateForm" onSubmit={handleSubmit}>
+            <input type="text" className="todoInput" onChange={handleChange} value={newValue}></input>
+            <button className="button" onClick={handleClick}>Editar</button>
+        </form>
+        );
+    }
+
+    function TodoElement(){{
+        return (
+        <div className="todoInfo">
+            {item.title} <button onClick={()=>setIsEdit(true)}>Editar</button>
+            <button>Eliminar</button>
+        </div>
+        );
+    }
+    }
+    //props: propiedades
+    return(
+        <div className ="todo">
+            {isEdit ? <FormEdit/>: <TodoElement/>}</div>
+    
+    );
+}
